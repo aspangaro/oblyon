29d78a8e253efea1f9f3ff9f37bdc667c7f26208
Alexandre SPANGARO <alexandre.spangaro@gmail.com>
2018-10-31 14:56:08 +0100

    Work on login page

--- htdocs/theme/oblyon/style.css.php
+++ htdocs/theme/oblyon/style.css.php
@@ -2243,6 +2243,109 @@ li.item-heading:hover > .sec-nav__link {
 /**
  * Login Block
  */
+.bodylogin
+{
+	background: #f0f0f0;
+	display: table;
+	position: absolute;
+	height: 100%;
+	width: 100%;
+}
+.login_center {
+	display: table-cell;
+	vertical-align: middle;
+}
+.login_vertical_align {
+	padding: 10px;
+	padding-bottom: 80px;
+}
+form#login {
+	padding-bottom: 30px;
+	font-size: 13px;
+	vertical-align: middle;
+}
+.login_table_title {
+	max-width: 530px;
+	color: #aaa !important;
+	padding-bottom: 20px;
+	/* text-shadow: 1px 1px 1px #FFF; */
+}
+.login_table label {
+	text-shadow: 1px 1px 1px #FFF;
+}
+.login_table {
+	margin: 0px auto;  /* Center */
+	padding-left:6px;
+	padding-right:6px;
+	padding-top:16px;
+	padding-bottom:12px;
+	max-width: 560px;
+
+	background-color: #FFFFFF;
+
+	-webkit-box-shadow: 0 4px 23px 5px rgba(0, 0, 0, 0.2), 0 2px 6px rgba(60,60,60,0.15);
+	box-shadow: 0 4px 23px 5px rgba(0, 0, 0, 0.2), 0 2px 6px rgba(60,60,60,0.15);
+
+	border-radius: 4px;
+	border:solid 1px rgba(80,80,80,.4);
+
+	border-top:solid 1px f8f8f8;
+}
+.login_table input#username, .login_table input#password, .login_table input#securitycode{
+	border: none;
+	border-bottom: solid 1px rgba(180,180,180,.4);
+	padding: 5px;
+	margin-left: 18px;
+	margin-top: 5px;
+}
+.login_table input#username:focus, .login_table input#password:focus, .login_table input#securitycode:focus {
+	outline: none !important;
+	/* box-shadow: none;
+	-webkit-box-shadow: 0 0 0 50px #FFF inset;
+	box-shadow: 0 0 0 50px #FFF inset;*/
+}
+.login_main_message {
+	text-align: center;
+	max-width: 570px;
+	margin-bottom: 10px;
+}
+.login_main_message .error {
+	border: 1px solid #caa;
+	padding: 10px;
+}
+div#login_left, div#login_right {
+	display: inline-block;
+	min-width: 245px;
+	padding-top: 10px;
+	padding-left: 16px;
+	padding-right: 16px;
+	text-align: center;
+	vertical-align: middle;
+}
+div#login_right select#entity {
+    margin-top: 10px;
+}
+table.login_table tr td table.none tr td {
+	padding: 2px;
+}
+table.login_table_securitycode {
+	border-spacing: 0px;
+}
+table.login_table_securitycode tr td {
+	padding-left: 0px;
+	padding-right: 4px;
+}
+#securitycode {
+	min-width: 60px;
+}
+#img_securitycode {
+	border: 1px solid #f4f4f4;
+}
+#img_logo, .img_logo {
+	max-width: 170px;
+	max-height: 90px;
+}
+
 div.login_block {
 	<?php if ($conf->global->MAIN_MENU_INVERT) { ?>
 		height: 40px;
