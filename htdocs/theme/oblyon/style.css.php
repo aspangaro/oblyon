40ab2e8dd3b925a64da07addbac1fd1c76d44ae6
Alexandre SPANGARO <aspangaro.dolibarr@gmail.com>
2020-04-02 21:03:54 +0200

    Fix select2

--- htdocs/theme/oblyon/style.css.php
+++ htdocs/theme/oblyon/style.css.php
@@ -6955,7 +6955,7 @@ div.dataTables_length select {
     background-color: #FFFFFF;
 }
 .select2-container--default .select2-selection--single .select2-selection__rendered {
-    color: unset;
+    /* color: unset; */
 }
 .select2-container .select2-choice {
     border-bottom: 1px solid #ccc;
