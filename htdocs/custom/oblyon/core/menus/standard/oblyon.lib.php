8a2c1acdf2d02e1253dbd9b8903042d43161db12
Thomas Negre <tnegre@open-dsi.fr>
2022-11-10 14:41:52 +0100

    Fix: the administration menu was accessible for unpriviledged users under dashboard dropdown.
    Similar to commit fbeca0f2dd2fa9b1766621c6f014b7253278daa0 . For some reason this did not happen locally, but did on client instance.

--- htdocs/custom/oblyon/core/menus/standard/oblyon.lib.php
+++ htdocs/custom/oblyon/core/menus/standard/oblyon.lib.php
@@ -824,7 +824,7 @@ function print_left_oblyon_menu($db, $menu_array_before, $menu_array_after, &$ta
 
             if (! empty($menu_invert)) $leftmenu= 'admintools';
 
-			if ($usemenuhider || empty($leftmenu) || preg_match('/^admintools/', $leftmenu)) {
+			if (!empty($user->admin) && ($usemenuhider || empty($leftmenu) || preg_match('/^admintools/', $leftmenu))) {
 				// Load translation files required by the page
 				$langs->loadLangs(array('admin', 'help', 'cron'));
 
