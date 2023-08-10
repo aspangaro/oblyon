6f2a77b63d579f9e666f5372c4ced1e44e28e4b1
Sylvain Legrand <sylvain.legrand@infras.fr>
2023-08-10 13:29:07 +0200

    Improve the use of variable (#131)
    * Improve the use of variable
    
    * Update global.inc.php
    
    * Update global.inc.php
    
    * Improve work in progress
    
    * Update global.inc.php
    
    * Infos
    
    * notifications
    
    * fixes
    
    * Update global.inc.php
    
    * Calendar + Tab
    
    * Update global.inc.php

--- htdocs/theme/oblyon/manifest.json.php
+++ htdocs/theme/oblyon/manifest.json.php
@@ -4,7 +4,7 @@
  * Copyright (C) 2007-2017	Regis Houssin			<regis.houssin@inodbox.com>
  * Copyright (C) 2011		Philippe Grand			<philippe.grand@atoo-net.com>
  * Copyright (C) 2012		Juanjo Menent			<jmenent@2byte.es>
- * Copyright (C) 2018       Ferran Marcet           <fmarcet@2byte.es>
+ * Copyright (C) 2018	   Ferran Marcet		   <fmarcet@2byte.es>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,16 +20,16 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 /**
- *		\file       htdocs/theme/oblyon/manifest.json.php
- *		\brief      File for The Web App
+ *		\file	   htdocs/theme/oblyon/manifest.json.php
+ *		\brief	  File for The Web App
  */
 //if (! defined('NOREQUIREUSER')) define('NOREQUIREUSER','1');	// Not disabled because need to load personalized language
 //if (! defined('NOREQUIREDB'))   define('NOREQUIREDB','1');	// Not disabled to increase speed. Language code is found on url.
-if (! defined('NOREQUIRESOC'))    define('NOREQUIRESOC', '1');
+if (! defined('NOREQUIRESOC'))	define('NOREQUIRESOC', '1');
 //if (! defined('NOREQUIRETRAN')) define('NOREQUIRETRAN','1');	// Not disabled because need to do translations
-if (! defined('NOCSRFCHECK'))     define('NOCSRFCHECK', 1);
+if (! defined('NOCSRFCHECK'))	 define('NOCSRFCHECK', 1);
 if (! defined('NOTOKENRENEWAL'))  define('NOTOKENRENEWAL', 1);
-if (! defined('NOLOGIN'))         define('NOLOGIN', 1);          // File must be accessed by logon page so without login
+if (! defined('NOLOGIN'))		 define('NOLOGIN', 1);		  // File must be accessed by logon page so without login
 //if (! defined('NOREQUIREMENU'))   define('NOREQUIREMENU',1);  // We need top menu content
 if (! defined('NOREQUIREHTML'))   define('NOREQUIREHTML', 1);
 if (! defined('NOREQUIREAJAX'))   define('NOREQUIREAJAX', '1');
@@ -40,15 +40,15 @@ $appli=constant('DOL_APPLICATION_TITLE');
 if (!empty($conf->global->MAIN_APPLICATION_TITLE)) $appli=$conf->global->MAIN_APPLICATION_TITLE;
 ?>
 {
-    "name": "<?php echo $appli; ?>",
-    "icons": [
-        {
-            "src": "<?php echo dol_buildpath('/theme/common/dolibarr_logo_256x256.png', 2); ?>",
-            "sizes": "256x256",
-            "type": "image/png"
-        }
-    ],
-    "theme_color": "#333",
-    "background_color": "#ffffff",
-    "display": "standalone"
+	"name": "<?php echo $appli; ?>",
+	"icons": [
+		{
+			"src": "<?php echo dol_buildpath('/theme/common/dolibarr_logo_256x256.png', 2); ?>",
+			"sizes": "256x256",
+			"type": "image/png"
+		}
+	],
+	"theme_color": "#333",
+	"background_color": "#ffffff",
+	"display": "standalone"
 }
