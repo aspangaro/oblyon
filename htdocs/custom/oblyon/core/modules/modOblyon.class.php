656e76addd7365caf7c3f4f6529806d086da4f7e
Alexandre SPANGARO <aspangaro.dolibarr@gmail.com>
2023-09-12 21:26:39 +0200

    New Logo

--- htdocs/custom/oblyon/core/modules/modOblyon.class.php
+++ htdocs/custom/oblyon/core/modules/modOblyon.class.php
@@ -73,7 +73,7 @@
 			$this->depends			= array();									// List of modules id that must be enabled if this module is enabled
 			$this->requiredby		= array();									// List of modules id to disable if this one is disabled
 			$this->conflictwith		= array();									// List of modules id this module is in conflict with
-			$this->phpmin			= array(5, 6);								// Minimum version of PHP required by module
+			$this->phpmin			= array(7, 0);								// Minimum version of PHP required by module
       		$this->need_dolibarr_version = array(14,0);							// Minimum version of Dolibarr required by module
 			$this->langfiles		= array($this->name.'@'.$this->name);
 			$this->const			= array();									// List of particular constants to add when module is enabled
