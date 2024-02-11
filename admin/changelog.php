eccc32189389d601068726b3d08282068aacc9be
Sylvain Legrand <sylvain.legrand@infras.fr>
2024-02-11 23:01:01 +0100

    Improve back list link

--- admin/changelog.php
+++ admin/changelog.php
@@ -50,7 +50,7 @@ $page_name = "ThemeOblyonChangelogTitle";
 llxHeader('', $langs->trans($page_name));
 
 // Subheader
-$linkback = '<a href="' . DOL_URL_ROOT . '/admin/modules.php">' . $langs->trans("BackToModuleList") . '</a>';
+$linkback = '<a href = "'.DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1">'.$langs->trans('BackToModuleList').'</a>';
 print load_fiche_titre($langs->trans($page_name), $linkback);
 
 // Configuration header
