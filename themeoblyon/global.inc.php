ac9f34f32c7f7dc37de279257a37b9ed9108ed44
Sylvain Legrand <sylvain.legrand@infras.fr>
2026-02-25 09:00:00 +0300

    Clean duplicates entries

--- themeoblyon/global.inc.php
+++ themeoblyon/global.inc.php
@@ -1130,11 +1130,6 @@ input.buttonpaymentcheque {
     background-repeat: no-repeat;
     background-position: 8px 7px;
 }
-input.buttonpaymentcheque {
-    background-image: url(<?php print dol_buildpath($path.'/paypal/img/object_paypal.png',1) ?>);
-    background-repeat: no-repeat;
-    background-position: 5px 4px;
-}
 input.buttonpaymentpaypal {
     background-image: url(<?php print dol_buildpath($path.'/paypal/img/object_paypal.png',1) ?>);
     background-repeat: no-repeat;
--- themeoblyon/modules/subtotal.inc.php
+++ themeoblyon/modules/subtotal.inc.php
@@ -6,11 +6,35 @@
 }
 
 .subtitleLevel2 {
-    background-color: <?php print (getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR') ? colorDarker(getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR'), 20) : '#ddddff'); ?> !important;
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR'), getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10)) : '#ddddff'); ?> !important;
 }
 
-.subtitleLevel3to9 {
-    background-color: <?php print (getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR') ? colorDarker(getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR'), 40) : '#eeeeff'); ?> !important;
+.subtitleLevel3 {
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 2)) : '#eeeeff'); ?> !important;
+}
+
+.subtitleLevel4 {
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 3)) : '#eeeeff'); ?> !important;
+}
+
+.subtitleLevel5 {
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 4)) : '#eeeeff'); ?> !important;
+}
+
+.subtitleLevel6 {
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 5)) : '#eeeeff'); ?> !important;
+}
+
+.subtitleLevel7 {
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 6)) : '#eeeeff'); ?> !important;
+}
+
+.subtitleLevel8 {
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 7)) : '#eeeeff'); ?> !important;
+}
+
+.subtitleLevel9 {
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_TITLE_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 8)) : '#eeeeff'); ?> !important;
 }
 
 .subtotalLevel1 {
@@ -18,9 +42,33 @@
 }
 
 .subtotalLevel2 {
-    background-color: <?php print (getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR') ? colorDarker(getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR'), 20) : '#ddddff'); ?> !important;
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 1)) : '#ddddff'); ?> !important;
 }
 
-.subtotalLevel3to9 {
-    background-color: <?php print (getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR') ? colorDarker(getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR'), 40) : '#eeeeff'); ?> !important;
+.subtotalLevel3 {
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 2)) : '#eeeeff'); ?> !important;
+}
+
+.subtotalLevel4 {
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 3)) : '#eeeeff'); ?> !important;
+}
+
+.subtotalLevel5 {
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 4)) : '#eeeeff'); ?> !important;
+}
+
+.subtotalLevel6 {
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 5)) : '#eeeeff'); ?> !important;
+}
+
+.subtotalLevel7 {
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 6)) : '#eeeeff'); ?> !important;
+}
+
+.subtotalLevel8 {
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 7)) : '#eeeeff'); ?> !important;
+}
+
+.subtotalLevel9 {
+    background-color: <?php print (getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR') ? colorLighten(getDolGlobalString('SUBTOTAL_SUBTOTAL_BACKGROUNDCOLOR'), (getDolGlobalInt('SUBTOTAL_TITLE_AND_SUBTOTAL_BRIGHTNESS_PERCENTAGE', 10) * 8)) : '#eeeeff'); ?> !important;
 }
